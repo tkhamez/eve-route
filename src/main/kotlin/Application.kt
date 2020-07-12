4540c1ed58b8a794b674353589188dc98883fac0
Tian Khamez <tkhamez@gmail.com>
2020-07-12 23:37:14 +0200

    Implemented EVE SSO (does not work with app engine because of environment variables).

--- src/main/kotlin/Application.kt
+++ src/main/kotlin/Application.kt
@@ -14,11 +14,11 @@ import io.ktor.auth.*
 import io.ktor.client.*
 import io.ktor.client.engine.apache.*
 import io.ktor.client.features.json.*
-//import io.ktor.client.request.*
-import kotlinx.coroutines.*
+import io.ktor.client.request.*
+//import kotlinx.coroutines.*
 import io.ktor.client.features.logging.*
-//import io.ktor.client.features.UserAgent
-import io.ktor.client.features.BrowserUserAgent
+import io.ktor.client.features.UserAgent
+import java.io.File
 
 //fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 fun main() {
@@ -35,55 +35,105 @@ fun main() {
 //fun Application.module(testing: Boolean = false) {
 fun Application.module() {
     install(Sessions) {
-        cookie<MySession>("MY_SESSION") {
-            cookie.extensions["SameSite"] = "lax"
+        cookie<Session>(
+                "SESSION",
+                directorySessionStorage(File(".sessions"), cached = true)
+        ) {
+            cookie.path = "/"
         }
     }
 
-    install(Authentication) {
-    }
-
-    @Suppress("UNUSED_VARIABLE")
-    val client = HttpClient(Apache) {
+    val httpClient = HttpClient(Apache) {
         install(JsonFeature) {
             serializer = GsonSerializer()
         }
         install(Logging) {
             level = LogLevel.HEADERS
         }
-        BrowserUserAgent() // install default browser-like user-agent
-    }
-    runBlocking {
-        // Sample for making a HTTP Client request
-        /*
-        val message = client.post<JsonSampleClass> {
-            url("http://127.0.0.1:8080/path/to/endpoint")
-            contentType(ContentType.Application.Json)
-            body = JsonSampleClass(hello = "world")
+        install(UserAgent) {
+            agent = "eve-route/0.0.1 Ktor http-client"
         }
-        */
+    }
+
+    install(Authentication) {
+        oauth("eve-oauth") {
+            client = httpClient
+            providerLookup = {
+                @Suppress("EXPERIMENTAL_API_USAGE")
+                OAuthServerSettings.OAuth2ServerSettings(
+                        name = "eve",
+                        authorizeUrl = "https://login.eveonline.com/v2/oauth/authorize",
+                        accessTokenUrl = "https://login.eveonline.com/v2/oauth/token",
+                        requestMethod = HttpMethod.Post,
+                        clientId = environment.config.property("eve.clientId").getString(),
+                        clientSecret = environment.config.property("eve.clientSecret").getString(),
+                        defaultScopes = listOf("esi-location.read_location.v1", "esi-ui.write_waypoint.v1")
+                )
+            }
+            urlProvider = {
+                @Suppress("EXPERIMENTAL_API_USAGE")
+                environment.config.property("eve.callback").getString()
+            }
+        }
     }
 
     routing {
         get("/") {
-            //println(testing)
-            call.respondText("HELLO WORLD!", contentType = ContentType.Text.Plain)
+            val session = call.sessions.get<Session>()
+            if (session?.eveAuth != null && session.eveAuth.containsKey("id")) {
+                //println(session.eveAuth)
+                call.respondText(
+                        "HI ${session.eveAuth["name"]}, <a href='/logout'>logout</a>",
+                        contentType = ContentType.Text.Html
+                )
+            } else {
+                call.respondText("<a href='/login'>login</a>", contentType = ContentType.Text.Html)
+            }
+        }
+
+        get("/logout") {
+            val session = call.sessions.get<Session>() ?: Session()
+            call.sessions.set(session.copy(eveAuth = emptyMap()))
+            call.respondRedirect("/")
+        }
+
+        authenticate("eve-oauth") {
+            route("/login") {
+                handle {
+                    val principal = call.authentication.principal<OAuthAccessTokenResponse.OAuth2>()
+                    if (principal != null) {
+                        val eveAuth = httpClient.get<EveAuth>("https://login.eveonline.com/oauth/verify") {
+                            header("Authorization", "Bearer ${principal.accessToken}")
+                        }
+                        if (eveAuth.CharacterID != null) {
+                            val session = call.sessions.get<Session>() ?: Session()
+                            call.sessions.set(session.copy(eveAuth = mapOf(
+                                    "id" to eveAuth.CharacterID,
+                                    "name" to eveAuth.CharacterName,
+                                    "accessToken" to principal.accessToken,
+                                    "scopes" to eveAuth.Scopes,
+                                    "expiresOn" to eveAuth.ExpiresOn,
+                                    "refreshToken" to principal.refreshToken
+                            )))
+                        }
+                    }
+                    call.respondRedirect("/")
+                }
+            }
         }
 
         // Static feature. Try to access `/static/ktor_logo.svg`
         static("/static") {
             resources("static")
         }
-
-        get("/session/increment") {
-            val session = call.sessions.get<MySession>() ?: MySession()
-            call.sessions.set(session.copy(count = session.count + 1))
-            call.respondText("Counter is ${session.count}. Refresh to increment.")
-        }
     }
 }
 
-data class MySession(val count: Int = 0)
+data class Session(val eveAuth: Map<String, Any?> = emptyMap())
 
-//data class JsonSampleClass(val hello: String)
-
+data class EveAuth(
+        val CharacterID: Long? = null,
+        val CharacterName: String? = null,
+        val ExpiresOn: String? = null,
+        val Scopes: String? = null
+)
