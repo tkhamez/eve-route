9b342d9b8796daa355539c5e9cfb4d9227cd7527
Tian Khamez <tkhamez@gmail.com>
2020-07-13 19:32:37 +0200

    Fixed test

--- test/ApplicationTest.kt
+++ test/ApplicationTest.kt
@@ -1,27 +1,29 @@
 package net.tkhamez.everoute
 
-import io.ktor.application.*
-import io.ktor.response.*
-import io.ktor.request.*
-import io.ktor.routing.*
+import com.typesafe.config.ConfigFactory
 import io.ktor.http.*
-import io.ktor.content.*
-import io.ktor.http.content.*
-import io.ktor.sessions.*
-import io.ktor.auth.*
-import io.ktor.client.*
-import io.ktor.client.engine.apache.*
-import io.ktor.client.features.json.*
-import io.ktor.client.request.*
-import kotlinx.coroutines.*
+import io.ktor.config.HoconApplicationConfig
 import kotlin.test.*
 import io.ktor.server.testing.*
+import org.junit.BeforeClass
 
 class ApplicationTest {
+    companion object {
+        @Suppress("EXPERIMENTAL_API_USAGE")
+        val engine = TestApplicationEngine(createTestEnvironment {
+            config = HoconApplicationConfig(ConfigFactory.load("application.conf"))
+        })
+        @BeforeClass @JvmStatic fun setup() {
+            engine.start(wait = false)
+            //engine.application.module()
+        }
+    }
+
     @Test
     fun testRoot() {
         //withTestApplication({ module(testing = true) }) {
-        withTestApplication({ module() }) {
+        //withTestApplication({ module() }) {
+        with(engine) {
             handleRequest(HttpMethod.Get, "/").apply {
                 assertEquals(HttpStatusCode.OK, response.status())
                 //assertEquals("HELLO WORLD!", response.content)
