8d0a370021b139f61e3a373fcc3e97f87c51cb6b
Tian Khamez <tkhamez@gmail.com>
2020-07-18 21:49:05 +0200

    Fixed reading of index.html.

--- src/main/kotlin/RoutesFrontend.kt
+++ src/main/kotlin/RoutesFrontend.kt
@@ -7,13 +7,12 @@ import io.ktor.http.content.static
 import io.ktor.response.respondText
 import io.ktor.routing.Route
 import io.ktor.routing.get
-import java.io.File
 
 fun Route.frontend() {
     get("/") {
-        val resource = Thread.currentThread().contextClassLoader.getResource("public/index.html")
+        val resource = javaClass.getResource("/public/index.html")
         if (resource != null) {
-            call.respondText(File(resource.file).readText(), contentType = ContentType.Text.Html)
+            call.respondText(resource.readText(), contentType = ContentType.Text.Html)
         } else {
             call.respondText("File not found.")
         }
