ea117ba42ebbfb60c808fe3ac8a8d041645a278f
Tian Khamez <tkhamez@gmail.com>
2020-08-24 18:07:05 +0200

    Title

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders title', () => {
   const { getByText } = render(<App />);
-  const titleElement = getByText(/Eve Route/i);
+  const titleElement = getByText(/EVE Route/i);
   expect(titleElement).toBeInTheDocument();
 });
